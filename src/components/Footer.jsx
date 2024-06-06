bbaae05fd54ef39ecd5ec2bc67aa6469ee58d024
MaohaB <leila.posonsby@gmail.com>
2024-06-06 08:19:49 +0200

    footer sass responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,14 @@
- 
+import './Footer.scss'
+import logo from "../images/LOGOblanc.png";
+const Footer = () => {
+    return (
+        <div className="footer">
+            <img className="logo" src={logo} alt="logo de kasa" />
+            <div className="desktop"><p>© 2020 Kasa. All rights reserved</p> </div>
+            <div className="mobile"><p>© 2020 Kasa. All
+            <br></br> rights reserved</p> </div>    
+            </div>
+    );
+} ;
+
+export default Footer; 
