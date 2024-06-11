d7f7bb9f2620135b1f2c66128b1a2feb8fd2d553
MaohaB <leila.posonsby@gmail.com>
2024-06-11 11:18:35 +0200

    lien logo vers accueil

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import Nav from "./Nav";
+import { Link } from "react-router-dom";
 import './Header.scss'
 import logo from "../images/LOGOgrnd.png";
 const Header = () => {
     return (
         <div className="header">
-            <img className="logo" src={logo} alt="logo de kasa" />
+            <Link to="/"><img className="logo" src={logo} alt="logo de kasa" /></Link>
             <Nav className="header-nav"/>       
         </div>
     );
