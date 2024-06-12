f975cbab8cddeaf211ce8e8534c8d950f9030c2a
MaohaB <leila.posonsby@gmail.com>
2024-06-12 21:09:58 +0200

    collapse animation & responsive

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,18 +1,30 @@
 import "./collapse.scss";
-import React from "react";
+import React, { useState } from "react";
+
+
 const Collapse = (props) => {
+  const [isContentvisible, setIsContentvisible] = useState(false);
+
+  const showContent = () => {
+    setIsContentvisible(!isContentvisible);
+  }
+
   return (
     <div className="Collapse-container">
-      <div className="Collapse Collapse-logement">
-        <div className="collapsetitle">
-          <span>{props.title}</span>
-          <span className="chevron"><i className="fa-solid fa-chevron-down rotate"></i></span>
 
+      <div className="Collapse Collapse-logement">
+        <div className="collapsetitle" onClick={showContent}>
+          <span>{props.title}</span>
+          <span className={isContentvisible ? "chevron-up" : "chevron-down"}><i className="fa-solid fa-chevron-down"></i></span>
+        
         </div>
+        
+        <span className={isContentvisible ?  "visible": "hidden" }>
         <div className="collapsecontent liste">
-          <ul>{props.content}</ul>
-        </div>
-      </div>
+         <ul>{props.content}</ul>
+        </div></span>
+
+    </div>
     </div>
   );
 };
--- src/components/Collapseabout.jsx
+++ src/components/Collapseabout.jsx
@@ -1,17 +1,24 @@
 import "./collapse.scss";
-import React from "react";
+import React, { useState } from "react";
+
 const Collapse = (props) => {
+    const [isContentvisible, setIsContentvisible] = useState(true);
+
+    const showContent = () => {
+      setIsContentvisible(!isContentvisible);
+    }
   return (
     <div className="Collapse-container-about">
       <div className="Collapse Collapse-about">
-        <div className="collapsetitle">
+        <div className="collapsetitle" onClick={showContent}>
           <span>{props.title}</span>
-          <span className="chevron"><i className="fa-solid fa-chevron-down rotate"></i></span>
+          <span className={isContentvisible ? "chevron-down" : "chevron-up"}><i className="fa-solid fa-chevron-down"></i></span>
 
         </div>
+        <span className={isContentvisible ? "hidden" : "visible"}>
         <div className="collapsecontent">
-          {props.content}
-        </div>
+         <ul>{props.content}</ul>
+        </div> </span>
       </div>
     </div>
   );
