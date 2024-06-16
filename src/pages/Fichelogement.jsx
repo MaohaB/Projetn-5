7e9d5121febcdb757f76bdd1c9258704e24d54cb
MaohaB <leila.posonsby@gmail.com>
2024-06-16 11:46:51 +0200

    redirection page 404 selon ID

--- src/pages/Fichelogement.jsx
+++ src/pages/Fichelogement.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { useParams, Navigate } from "react-router-dom";
 import logements from "../data/logements.json";
 import "../components/Fichelogement.scss"
@@ -10,20 +10,15 @@ import Situation from "../components/Situation"
 import Collapse from "../components/Collapse"
 
 
-const findID = (id) =>{
-    return logements.find((logement)=>logement.id === id);
-}
-// function Fichelogement(logements) {
-//     const { id } = useParams();
-//     return ( (logement) => {
-//     const selectedappertment = logement.find((logement)=>logement.id === id);
-//     if (!selectedappertment) {
-//        return <Navigate to="/404"/>;
-//     }
-
 const Logement = () => {
+
     const{id} = useParams();
-    const logement = findID(id)
+    const logement = logements.find((logement)=>logement.id === id);
+    if (!logement) {
+        return <Navigate to="/404"/>;
+    }
+
+
     const host= logement.host
     const name = logement.host.name
     const [firstname, lastname] = name.split(" ");
@@ -53,5 +48,6 @@ const Logement = () => {
         </div>
     );
 } ;
+
 
 export default Logement;  
