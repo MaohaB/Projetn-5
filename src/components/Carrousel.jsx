3d28cfd2f78b4ae1019608e155aeecc5386d4a0e
MaohaB <leila.posonsby@gmail.com>
2024-06-27 09:25:46 +0200

    carroussel loop

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -1,30 +1,45 @@
 
 import { useState } from "react";
 import "./carroussel.scss"
-const Carrousel = ({imgUrl, pictures}) => {
-    const [current, setCurrent] = useState(0);
-    const enoughPictures = () => {
-        return pictures && pictures.length > 0;
-    };
+const Carrousel = ({pictures}) => {
+    const [currentPicture, setCurrentPicture] = useState(0);
+    //const enoughPictures = () => { return pictures && pictures.length > 0;};
     console.log("nombre d'images= " + pictures.length)
+    const getClassName = (i) => {
+        if (i === currentPicture) return "show-picture";
+        return "";
+    };
+
+    const nextImg =() => {
+        setCurrentPicture((currentPicture + 1) % pictures.length);
+    }
+    const previousImg =() => {
+        const newCurrentPicture = currentPicture - 1;
+        if (newCurrentPicture < 0) {
+        setCurrentPicture(pictures.length - 1);
+        return;
+        }
+        setCurrentPicture((currentPicture - 1) % pictures.length);
+    }
     return (
         <div className="Carrousel">
             <div className="slider-arrows">
-            {pictures.length > 1 ? <div className="arrow arrow-previous" //onClick={previousImg}
+            {pictures.length > 1 ? <div className="arrow arrow-previous" onClick={previousImg}
             >
             <i className="fa-solid fa-chevron-left"></i>
             </div>:<span></span>}
 
-            {pictures.length > 1 ? <div className="arrow arrow-next" //onClick={nextImg}
+            {pictures.length > 1 ? <div className="arrow arrow-next" onClick={nextImg}
             >
             <i className="fa-solid fa-chevron-right"></i>
             </div>:<span></span>}
             </div>
             
             <div className="nbImg"><span>
-                test / {pictures.length}</span></div>
-
-            <img src={imgUrl} alt=""/>
+                {currentPicture + 1} / {pictures.length}</span></div>
+            {pictures.map((pic, i) => ( 
+            <img key={pic} src={pic} alt="" className={getClassName(i)} />
+            ))}
         </div>
     );
 } ;
